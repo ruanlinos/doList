e0ac42a4888146eef6f9ce8e9b8d39a1106b1df0
merisio <ruan.compras@hotmail.com>
2020-03-05 18:37:42 -0300

    feat: created styles.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,91 +1,30 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import { MdAddCircle, MdClose, MdEventAvailable } from 'react-icons/md';
+import { MdAddCircle, MdClose } from 'react-icons/md';
 import colors from '../../styles/colors';
 import Constants from '../../constants';
+import {
+  Container,
+  Wrapper,
+  Author,
+  Person,
+  Title,
+  Content,
+  SubTitle,
+  Filters,
+  Todo,
+  CreatedAt,
+  Calendar,
+  Footer,
+  Modal,
+  Button,
+} from './styles';
 
-const Container = styled.div`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-`;
-export const Wrapper = styled.div`
-  max-width: 600px;
-`;
-const Title = styled.h1`
-  margin: 30px 0 20px 0;
-`;
-const SubTitle = styled.h2``;
-
-export const Author = styled.div`
-  display: flex;
-  align-items: center;
-  color: ${colors.DarkGray};
-`;
-export const Person = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 140px;
-  height: 31px;
-  border: 3px solid #eae9f2;
-  box-sizing: border-box;
-  border-radius: 34px;
-  margin-left: 6px;
-  p {
-    font-weight: bold;
-    font-size: 12px;
-    line-height: 15px;
-    color: #45475d;
-  }
-`;
-export const Content = styled.div``;
-export const Footer = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-export const Todo = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
-  p {
-    font-weight: 600;
-    font-size: 18px;
-    line-height: 23px;
-  }
-`;
-export const CreatedAt = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  small {
-    font-weight: 600;
-    font-size: 12px;
-    line-height: 15px;
-    color: ${colors.DarkGray};
-  }
-`;
-const Calendar = styled(MdEventAvailable)`
-  margin: 0 20px 0 5px;
-`;
-const Filters = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 24px 0 56px 0;
-  p {
-    &:nth-child(2) {
-      margin: 0 50px;
-    }
-  }
-`;
 const Home: React.FC = () => {
   const [todos, setTodos] = useState(Constants);
-
+  const [modalState, setModalState] = useState(false);
+  const handleModalState = (params: boolean) => {
+    setModalState(params);
+  };
   return (
     <Container>
       <Wrapper>
@@ -115,9 +54,18 @@ const Home: React.FC = () => {
           ))}
         </Content>
         <Footer>
-          <MdAddCircle color="#331CBF" size={40} />
+          <Button onClick={() => handleModalState(true)}>
+            <MdAddCircle color="#331CBF" size={40} />
+          </Button>
         </Footer>
       </Wrapper>
+      <Modal
+        footer={[]}
+        visible={modalState}
+        onCancel={() => handleModalState(false)}
+      >
+        <p> isso é uma modal</p>
+      </Modal>
     </Container>
   );
 };
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,13 +1,106 @@
 import styled from 'styled-components';
+import { MdEventAvailable } from 'react-icons/md';
+import { Modal as AntdModal } from 'antd';
+import colors from '../../styles/colors';
 
 export const Container = styled.div`
-  width: 100%;
-  min-height: 100vh;
   display: flex;
-  padding: 0 20px;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+export const Wrapper = styled.div`
+  max-width: 600px;
+`;
+export const Title = styled.h1`
+  margin: 30px 0 20px 0;
+`;
+export const SubTitle = styled.h2``;
 
-  @media only screen and (max-width: 56.25em) {
-    flex-direction: column-reverse;
-    padding: 1rem;
+export const Author = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${colors.DarkGray};
+`;
+export const Person = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 140px;
+  height: 31px;
+  border: 3px solid #eae9f2;
+  box-sizing: border-box;
+  border-radius: 34px;
+  margin-left: 6px;
+  p {
+    font-weight: bold;
+    font-size: 12px;
+    line-height: 15px;
+    color: #45475d;
+    margin-bottom: 0;
   }
+`;
+export const Content = styled.div``;
+export const Footer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  button {
+    width: 40px;
+    height: 40px;
+    border: none;
+    background: none !important;
+  }
+`;
+export const Todo = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  p {
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 23px;
+  }
+`;
+export const CreatedAt = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  small {
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 15px;
+    color: ${colors.DarkGray};
+  }
+`;
+export const Calendar = styled(MdEventAvailable)`
+  margin: 0 20px 0 5px;
+`;
+export const Filters = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 24px 0 56px 0;
+  p {
+    &:nth-child(2) {
+      margin: 0 50px;
+    }
+  }
+`;
+
+export const Modal = styled(AntdModal)`
+  .ant-modal-header {
+    border-bottom: none;
+  }
+  .ant-modal-footer {
+    border-top: none;
+  }
+`;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
